fcf32c6ef8d5534fd2d423835359590161254bea
Dave Shevitz <shevitz@stripe.com>
2023-10-18 21:47:06 -0700

    refactor code

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ let playerTurn = "x";
 let moves = 0;
 let isGameOver = false;
 let span = document.getElementsByTagName("span");
-let restartButton = `<button onclick="playAgain()">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
-                        <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
-                        <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
-                        </svg>
-                        </button>`;
+let restartButton = createRestartButton();
 
 
 function play(y){
@@ -42,11 +37,11 @@ function checkWinner(a, b, c) {
     a--;
     b--;
     c--;
-    if ((span[a].dataset.player === span[b].dataset.player) && (span[b].dataset.player === span[c].dataset.player) && (span[a].dataset.player === span[c].dataset.player) && (span[a].dataset.player === "x" || span[a].dataset.player === "o") && isGameOver == false) {
+    const playerMatch = (span[a].dataset.player === span[b].dataset.player) && 
+                        (span[b].dataset.player === span[c].dataset.player) 
+    if (playerMatch && span[a].dataset.player !== "none" && isGameOver == false) {
         // highlight the winning squares!
-        span[a].parentNode.className += " activeBox";
-        span[b].parentNode.className += " activeBox";
-        span[c].parentNode.className += " activeBox";
+        [a, b, c].forEach(index => span[index].parentNode.classList.add("activeBox"));
         gameOver(a);
     }
 }
@@ -58,22 +53,26 @@ function gameOver(a) {
                                  Player ${span[a].dataset.player.toUpperCase() } + Wins!!!
                                 <br>
                                 <br>
-                                ${restartButton}`
+                                ${restartButton}`;
     let div = document.createElement("div");
     div.className = "alert";
     div.innerHTML = gameOverAlertElement;
-    document.getElementsByTagName("body")[0].appendChild(div);
+    document.appendChild(div);
     isGameOver = true;
     moves = 0;
 }
 
 function playAgain() {
-    document.getElementsByClassName("alert")[0].parentNode.removeChild(document.getElementsByClassName("alert")[0]);
+    const alertElement = document.querySelector(".alert");
+    const elementsWithActiveBox = document.querySelectorAll("activeBox");
+    if (alertElement) {
+        alertElement.remove();
+    }
     resetGame();
     isGameOver = false;
-    for (let i = 0; i < span.length; i++){
-        span[i].parentNode.className = span[i].parentNode.className.replace("activeBox", "");
-    }
+    elementsWithActiveBox.forEach(element => {
+        element.classList.remove("activeBox");
+    });
 }
 
 function resetGame(){
@@ -85,7 +84,7 @@ function resetGame(){
 }
 
 function draw() {
-    let drawAlertElement = `<b>DRAW!!||</b>
+    let drawAlertElement = `<b>DRAW!!</b>
                             <br>
                             <br>
                             ${restartButton}`
@@ -95,5 +94,27 @@ function draw() {
     document.getElementsByTagName("body")[0].appendChild(div);
     isGameOver = true;
     moves = 0;
+}
+
+function gameOverElement() {
+    
+}
+
+function createRestartButton() {
+    const button = document.createElement("button");
+    button.addEventListener("click", playAgain);
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "30");
+    svg.setAttribute("height", "30");
+    svg.setAttribute("fill", "currentColor");
+    svg.setAttribute("class", "bi bi-arrow-repeat");
+    svg.setAttribute("viewBox", "0 0 16 16");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z");
+    
+    button.appendChild(svg);
+    svg.appendChild(path);
+    
+    return button;
 }
 
